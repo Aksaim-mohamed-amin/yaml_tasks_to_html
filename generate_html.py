d5e7717760aed88faf4e1bee1e3f75a283292846
Aksaim Mohamed amin <112438572+Aksaim-mohamed-amin@users.noreply.github.com>
2024-12-06 00:45:42 +0000

    create the py script to render html page from yaml file

--- generate_html.py
+++ generate_html.py
@@ -1,1 +1,29 @@
+import yaml
+from jinja2 import Template
 
+# Load tasks from the YAML file
+with open('tasks.yaml', 'r') as file:
+    data = yaml.safe_load(file)
+
+# Simple HTML template
+template = Template("""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>To-Do Tasks</title>
+</head>
+<body>
+    <h1>Task List</h1>
+    <ul>
+        {% for task in tasks %}
+        <li>{{ task.title }} - status: {{ task.status }}</li>
+        {% endfor %}
+    </ul>
+</body>
+</html>
+""")
+
+# Render and save to index.html
+html_output = template.render(tasks=data['tasks'])
+with open("templates/index.html", "w") as output_file:
+    output_file.write(html_output)
